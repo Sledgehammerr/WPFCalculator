3c4a3e09a41af313222621545b448ac7ec61de28
fdsaf asdfa <supervalera9@gmail.com>
2020-06-15 22:56:16 +0300

    WPFCalculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    delegate void CaclulatorUpdateOutput(Calculator sender, double value, int precision);
+    delegate void CaclulatorInernalError(Calculator sender, string message);
+    enum CalculatorOperation { Add, Sub, Mul, Div}
+    enum CalculatorTransformation { Negate, Percent, Sqr, Sqrt, Inverse }
+    class Calculator
+    {
+        double? input = null;
+        double? result = null;
+
+        CalculatorOperation? op = null;
+
+        bool hasPoint;
+        int fractionDigits = 0;
+
+        public event CaclulatorUpdateOutput DidUpdateValue;
+        public event CaclulatorInernalError InputError;
+        public event CaclulatorInernalError ComputationError;
+
+        public void AddDigit(int digit)
+        {
+            if (hasPoint == false)
+            {
+                if (input.HasValue && Math.Log10(input.Value) > 9)
+                {
+                    InputError?.Invoke(this, "Value is too long for this calculator!");
+                    return;
+                }
+
+                input = (input ?? 0) * 10 + digit;                
+            }
+            else
+            {
+                if (fractionDigits > 8)
+                {
+                    InputError?.Invoke(this, "Value is too long for this calculator!");
+                    return;
+                }
+                fractionDigits += 1;
+                input = (input ?? 0) + digit * Math.Pow(10, -fractionDigits);
+            }
+            DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+        }
+
+        public void AddPoint()
+        {
+            hasPoint = true;
+        }
+
+        public void TransformInput(CalculatorTransformation t)
+        {
+            input = input ?? 0;
+
+            switch (t)
+            {
+                case CalculatorTransformation.Negate:
+                    input = -input;
+                    break;
+                case CalculatorTransformation.Percent:
+                    input /= 100;
+                    break;
+                case CalculatorTransformation.Sqr:
+                    input *= input;
+                    break;
+                case CalculatorTransformation.Sqrt:
+                    if (input.HasValue && input.Value >= 0)
+                        input = Math.Sqrt(input ?? 0);
+                    else
+                        ComputationError.Invoke(this, "Negative Square");
+                    break;
+                case CalculatorTransformation.Inverse:
+                    if (input.HasValue && input.Value != 0)
+                        input = 1 / input;
+                    else
+                        ComputationError.Invoke(this, "Division by Zero");
+                    break;
+            }
+
+            DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+        }
+
+        public void AddOperation(CalculatorOperation op)
+        {
+            if (this.op.HasValue)
+                Compute();
+
+            this.op = op;
+            if (input.HasValue)
+            {
+                result = input;
+                this.Clear();
+            }
+        }
+
+        public void Compute()
+        {
+            switch (this.op)
+            {
+                case CalculatorOperation.Add:
+                    result += input;
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    ResetInput();
+                    break;
+                case CalculatorOperation.Sub:
+                    result -= input;
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    ResetInput();
+                    break;
+                case CalculatorOperation.Mul:
+                    result *= input;
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    ResetInput();
+                    break;
+                case CalculatorOperation.Div:
+                    if (input.HasValue && input.Value != 0)
+                    {
+                        result /= input;
+                        DidUpdateValue?.Invoke(this, result.Value, 0);
+                        ResetInput();
+                    }
+                    else
+                    {
+                        ComputationError.Invoke(this, "Division by Zero");
+                    }
+                    break;
+            }
+        }
+
+        public void Delete()
+        {
+            if (!hasPoint)
+            {
+                input = (int)(input ?? 0) / 10;
+            }
+            else
+            {
+                input = ((int)((input ?? 0) / Math.Pow(10, -fractionDigits)) / 10) * Math.Pow(10, -(--fractionDigits));
+                if (fractionDigits == 0)
+                {
+                    hasPoint = false;
+                }
+            }
+            DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+        }
+
+        void ResetInput()
+        {
+            input = null;
+            fractionDigits = 0;
+            hasPoint = false;
+        }
+
+        public void Clear()
+        {
+            ResetInput();
+            DidUpdateValue?.Invoke(this, 0, 0);
+        }
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsFormsApp1;
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+            calc.DidUpdateValue += Calc_DidUpdateValue;
+        }
+
+        private void Calc_DidUpdateValue(Calculator sender, double value, int precision)
+        {
+            output.Text = value.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            int digit = -1;
+            if (int.TryParse(button.Content.ToString(), out digit))
+            {
+                calc.AddDigit(digit);
+            }
+            else
+            {
+                switch (button.Tag)
+                {
+                    case "plus":
+                        calc.AddOperation(CalculatorOperation.Add);
+                        break;
+                    case "minus":
+                        calc.AddOperation(CalculatorOperation.Sub);
+                        break;
+                    case "multiplication":
+                        calc.AddOperation(CalculatorOperation.Mul);
+                        break;
+                    case "division":
+                        calc.AddOperation(CalculatorOperation.Div);
+                        break;
+                    case "negate":
+                        calc.TransformInput(CalculatorTransformation.Negate);
+                        break;
+                    case "percent":
+                        calc.TransformInput(CalculatorTransformation.Percent);
+                        break;
+                    case "sqr":
+                        calc.TransformInput(CalculatorTransformation.Sqr);
+                        break;
+                    case "sqrt":
+                        calc.TransformInput(CalculatorTransformation.Sqrt);
+                        break;
+                    case "inverse":
+                        calc.TransformInput(CalculatorTransformation.Inverse);
+                        break;
+                    case "point":
+                        calc.AddPoint();
+                        break;
+                    case "compute":
+                        calc.Compute();
+                        break;
+                    case "delete":
+                        calc.Delete();
+                        break;
+                    case "clear":
+                        calc.Clear();
+                        break;
+                    default:
+                        MessageBox.Show(button.Tag?.ToString() ?? "");
+                        break;
+                }
+            }
+        }
+    }
+}
